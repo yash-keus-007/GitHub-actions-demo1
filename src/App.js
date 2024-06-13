96424fdedf919da7930b8a3014cf1bcf771967c6
yash-keus-007 <yashkumar.sharma@keus.in>
2024-06-13 18:35:02 +0530

    file change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App () {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yash Sharma
+          Keus
         </p>
         <a
           className="App-link"
